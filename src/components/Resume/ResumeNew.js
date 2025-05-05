afdc4fdebe06fb4aa8387dcb2148b1a9527e3682
Shravyachennuri12 <chennurishravya088@gmail.com>
2025-05-05 13:04:22 +0530

    Updated pdf

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/ChennuruShravya Resume(Updated).pdf";
+import pdf from "../../Assets/../Assets/ChennuruShravya_Resume(updated).pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
